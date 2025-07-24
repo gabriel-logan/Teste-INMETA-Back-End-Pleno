0a00491ff63a8854a279f085c563aa7ad83472e6
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 05:32:27 -0300

    feat: add PublicDocumentsTypeResponseDto and update controller and service methods to use it

--- src/documents-types/controllers/documents-types.controller.ts
+++ src/documents-types/controllers/documents-types.controller.ts
@@ -10,6 +10,7 @@ import {
 
 import { CreateDocumentsTypeRequestDto } from "../dto/request/create-documents-type.dto";
 import { UpdateDocumentsTypeRequestDto } from "../dto/request/update-documents-type.dto";
+import { PublicDocumentsTypeResponseDto } from "../dto/response/public-documents-type.dto";
 import { DocumentsTypesService } from "../providers/documents-types.service";
 
 @Controller("documents-types")
@@ -17,17 +18,21 @@ export class DocumentsTypesController {
   constructor(private readonly documentsTypesService: DocumentsTypesService) {}
 
   @Get()
-  async findAll() {
+  async findAll(): Promise<PublicDocumentsTypeResponseDto[]> {
     return await this.documentsTypesService.findAll();
   }
 
   @Get(":id")
-  async findById(@Param("id") id: string) {
+  async findById(
+    @Param("id") id: string,
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return await this.documentsTypesService.findById(id);
   }
 
   @Post()
-  async create(@Body() createDocumentsTypeDto: CreateDocumentsTypeRequestDto) {
+  async create(
+    @Body() createDocumentsTypeDto: CreateDocumentsTypeRequestDto,
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return await this.documentsTypesService.create(createDocumentsTypeDto);
   }
 
@@ -35,12 +40,14 @@ export class DocumentsTypesController {
   async update(
     @Param("id") id: string,
     @Body() updateDocumentsTypeDto: UpdateDocumentsTypeRequestDto,
-  ) {
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return await this.documentsTypesService.update(id, updateDocumentsTypeDto);
   }
 
   @Delete(":id")
-  async delete(@Param("id") id: string) {
+  async delete(
+    @Param("id") id: string,
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return await this.documentsTypesService.delete(id);
   }
 }
--- src/documents-types/providers/documents-types.service.ts
+++ src/documents-types/providers/documents-types.service.ts
@@ -2,29 +2,32 @@ import { Injectable } from "@nestjs/common";
 
 import { CreateDocumentsTypeRequestDto } from "../dto/request/create-documents-type.dto";
 import { UpdateDocumentsTypeRequestDto } from "../dto/request/update-documents-type.dto";
+import { PublicDocumentsTypeResponseDto } from "../dto/response/public-documents-type.dto";
 
 @Injectable()
 export class DocumentsTypesService {
-  async findAll() {
+  async findAll(): Promise<PublicDocumentsTypeResponseDto[]> {
     return `This action returns all documentsTypes`;
   }
 
-  async findById(id: string) {
+  async findById(id: string): Promise<PublicDocumentsTypeResponseDto> {
     return `This action returns a #${id} documentsType`;
   }
 
-  async create(createDocumentsTypeDto: CreateDocumentsTypeRequestDto) {
+  async create(
+    createDocumentsTypeDto: CreateDocumentsTypeRequestDto,
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return "This action adds a new documentsType";
   }
 
   async update(
     id: string,
     updateDocumentsTypeDto: UpdateDocumentsTypeRequestDto,
-  ) {
+  ): Promise<PublicDocumentsTypeResponseDto> {
     return `This action updates a #${id} documentsType`;
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<PublicDocumentsTypeResponseDto> {
     return `This action removes a #${id} documentsType`;
   }
 }
