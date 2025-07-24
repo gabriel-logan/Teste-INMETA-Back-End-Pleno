4c1e17a46474f9aecdd478b4bbdf507705b469a5
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 01:51:51 -0300

    feat: implement Swagger documentation setup with operations sorting

---
+++ src/configs/swagger.ts
@@ -0,0 +1,42 @@
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import type { OpenAPIObject } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+type OperationsSorterParam = {
+  get: (type: string) => string;
+};
+
+function operationsSorter(
+  a: OperationsSorterParam,
+  b: OperationsSorterParam,
+): number {
+  const methodOrder = ["get", "post", "patch", "put", "delete"];
+  const methodA = methodOrder.indexOf(a.get("method").toLowerCase());
+  const methodB = methodOrder.indexOf(b.get("method").toLowerCase());
+  return methodA - methodB;
+}
+
+const config = new DocumentBuilder()
+  .setTitle("API V1 Gateway")
+  .setDescription("The API V1 Gateway description")
+  .setVersion("1.0.0")
+  .addBearerAuth()
+  .addSecurityRequirements("bearer")
+  .build();
+
+export default function swaggerInitializer(
+  app: NestExpressApplication,
+  configs: { globalPrefix: string },
+): void {
+  function documentFactory(): OpenAPIObject {
+    return SwaggerModule.createDocument(app, config);
+  }
+
+  SwaggerModule.setup(`${configs.globalPrefix}/docs`, app, documentFactory, {
+    customSiteTitle: "API V1 Gateway Documentation",
+    customCss: `.swagger-ui .opblock.opblock-patch .opblock-summary-method { background:#d1bd21; }`,
+    swaggerOptions: {
+      operationsSorter,
+    },
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
 import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
 
 import { AppModule } from "./app.module";
+import swaggerInitializer from "./configs/swagger";
 import type { EnvGlobalConfig } from "./configs/types";
 
 const logger = new Logger("Bootstrap");
 
+const globalPrefix = "api/v1";
+
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix(globalPrefix);
 
   const configService = app.get(ConfigService<EnvGlobalConfig, true>);
 
   const { nodeEnv, baseUrl, port } =
     configService.get<EnvGlobalConfig["server"]>("server");
 
+  // Initialize Swagger
+  swaggerInitializer(app, { globalPrefix });
+
   await app.listen(port);
 
-  logger.log(`Application is running on: ${baseUrl}`);
+  logger.log(`Application is running on: ${baseUrl}/${globalPrefix}`);
+  logger.log(
+    `Access the Swagger documentation at: ${baseUrl}/${globalPrefix}/docs`,
+  );
   logger.log(`Environment: ${nodeEnv}`);
 }
 
