55384b11e54393a4b6f3c5ab80b7a429973ff1ab
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 08:42:15 -0300

    feat: set url property to null when creating a new document

--- src/documents/providers/documents.service.ts
+++ src/documents/providers/documents.service.ts
@@ -60,6 +60,7 @@ export class DocumentsService {
     const newDocument = new this.documentModel({
       documentType: documentType.id,
       status,
+      url: null,
     });
 
     const savedDocument = await newDocument.save();
