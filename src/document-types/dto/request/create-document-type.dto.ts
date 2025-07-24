fef7fc67ae9fb59a819508a258309ffea906e6ac
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 07:06:50 -0300

    feat: update CreateDocumentTypeRequestDto to use enum for document type validation

--- src/document-types/dto/request/create-document-type.dto.ts
+++ src/document-types/dto/request/create-document-type.dto.ts
@@ -1,6 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum } from "class-validator";
+
+export enum DocumentTypeAllowedValues {
+  CPF = "CPF",
+  CNPJ = "CNPJ",
+  CTPS = "CTPS",
+  RG = "RG",
+  CNH = "CNH",
+}
 
 export class CreateDocumentTypeRequestDto {
-  @ApiProperty()
-  public name: string;
+  @ApiProperty({
+    enum: DocumentTypeAllowedValues,
+    description: "The name of the document type",
+    example: DocumentTypeAllowedValues.CPF,
+  })
+  @IsEnum(DocumentTypeAllowedValues)
+  public name: DocumentTypeAllowedValues;
 }
