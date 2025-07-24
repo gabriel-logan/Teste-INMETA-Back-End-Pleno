a21c558594dd01c21ec901fbc27fdb49d16d80db
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 02:13:38 -0300

    feat: add IsCpf decorator for CPF validation using multiform-validator

---
+++ src/common/decorators/validation/IsCpf.ts
@@ -0,0 +1,31 @@
+import type { ValidationOptions } from "class-validator";
+import { buildMessage, ValidateBy } from "class-validator";
+import { cpfIsValid } from "multiform-validator";
+
+export const IS_CPF = "isCpf";
+
+export function isCpf(value: unknown): boolean {
+  if (typeof value !== "string" && typeof value !== "number") {
+    return false; // Ensure the value is a string or number
+  }
+
+  return cpfIsValid(value.toString()).isValid;
+}
+
+export function IsCpf(
+  validationOptions?: ValidationOptions,
+): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: IS_CPF,
+      validator: {
+        validate: (value): boolean => isCpf(value),
+        defaultMessage: buildMessage(
+          (eachPrefix) => eachPrefix + "$property must be a valid CPF",
+          validationOptions,
+        ),
+      },
+    },
+    validationOptions,
+  );
+}
