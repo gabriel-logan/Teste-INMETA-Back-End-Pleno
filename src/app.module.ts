3b96b0bb87673d7b713a9066bcddc14948b8d167
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 02:25:52 -0300

    feat: integrate MongooseModule for MongoDB connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
+import envDatabase from "./configs/env.database";
 import envGlobal from "./configs/env.global";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [envGlobal],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forFeature(envDatabase)],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>("database.mongodb.uri"),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
