cfe1cd7f82f4df0bd36ff80e6ac6062fd63ced8c
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 06:14:44 -0300

    feat: add DocumentStatus enum and update Document schema properties

--- src/documents/schemas/document.schema.ts
+++ src/documents/schemas/document.schema.ts
@@ -1,12 +1,26 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { DocumentsType } from "src/documents-types/schemas/documents-type.schema";
 
 export type DocumentDocument = HydratedDocument<Document>;
+
+export enum DocumentStatus {
+  MISSING = "missing",
+  PUBLISHED = "published",
+  ARCHIVED = "archived",
+  DELETED = "deleted",
+}
 
 @Schema({ timestamps: true })
 export class Document {
   @Prop({ required: true })
   public name: string;
+
+  @Prop({ required: true })
+  public status: DocumentStatus;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: DocumentsType.name })
+  public documentsType: DocumentsType;
 
   public createdAt: Date;
   public updatedAt: Date;
