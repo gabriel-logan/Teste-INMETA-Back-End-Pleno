80ebb0c72aaa44b719894605b8213d8a4ad8194a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 02:36:20 -0300

    feat: add ApiProperty decorator to CreateEmployeeRequestDto for Swagger documentation

--- src/employees/dto/request/create-employee.dto.ts
+++ src/employees/dto/request/create-employee.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotBlankString } from "src/common/decorators/validation/IsNotBlankString";
 
 export class CreateEmployeeRequestDto {
+  @ApiProperty()
   @IsNotBlankString()
   public name: string;
 }
