c6372314a6dd7957a903e57f97b4e1766f27b2c1
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 02:09:54 -0300

    feat: implement IsNotBlankString decorator for string validation

---
+++ src/common/decorators/validation/IsNotBlankString.ts
@@ -0,0 +1,30 @@
+import type { ValidationOptions } from "class-validator";
+import { buildMessage, ValidateBy } from "class-validator";
+
+export const IS_NOT_BLANK_STRING = "isNotBlankString";
+
+export function isNotBlankString(value: unknown): boolean {
+  if (typeof value !== "string") {
+    return false; // Ensure the value is a string
+  }
+
+  return value.trim().length > 0;
+}
+
+export function IsNotBlankString(
+  validationOptions?: ValidationOptions,
+): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: IS_NOT_BLANK_STRING,
+      validator: {
+        validate: (value): boolean => isNotBlankString(value),
+        defaultMessage: buildMessage(
+          (eachPrefix) => eachPrefix + "$property should be a non-blank string",
+          validationOptions,
+        ),
+      },
+    },
+    validationOptions,
+  );
+}
