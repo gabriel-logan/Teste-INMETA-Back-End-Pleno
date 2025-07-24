00e37f0e7da377e0d6f3f96e0b456f571639526a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 01:57:48 -0300

    refactor: extract NODE_ENV assignment to a constant for clarity

--- src/configs/env.global.ts
+++ src/configs/env.global.ts
@@ -1,8 +1,12 @@
 import type { EnvGlobalConfig } from "./types";
 
+const nodeEnv = process.env.NODE_ENV as
+  | EnvGlobalConfig["server"]["nodeEnv"]
+  | undefined;
+
 export default (): EnvGlobalConfig => ({
   server: {
-    nodeEnv: process.env.NODE_ENV ?? "production",
+    nodeEnv: nodeEnv ?? "production",
 
     baseUrl: process.env.BASE_URL!,
 
