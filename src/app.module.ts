da25dfd76bc2a2a77d4ef105a227850b5cb5a15e
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 06:51:58 -0300

    refactor: re-implement DocumentTypes module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import envDatabase from "./configs/env.database";
 import envGlobal from "./configs/env.global";
+import { DocumentTypesModule } from "./document-types/document-types.module";
 import { DocumentsModule } from "./documents/documents.module";
 import { DocumentsTypesModule } from "./documents-types/documents-types.module";
 import { EmployeesModule } from "./employees/employees.module";
@@ -24,6 +25,7 @@ import { EmployeesModule } from "./employees/employees.module";
     EmployeesModule,
     DocumentsModule,
     DocumentsTypesModule,
+    DocumentTypesModule,
   ],
   controllers: [AppController],
   providers: [],
