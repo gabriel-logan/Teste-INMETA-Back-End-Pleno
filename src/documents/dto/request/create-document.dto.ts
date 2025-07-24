0916ed3e9a45b5aa7bd33ed61a0f726d9d44d957
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 06:25:23 -0300

    feat: add validation decorators to CreateDocumentRequestDto

--- src/documents/dto/request/create-document.dto.ts
+++ src/documents/dto/request/create-document.dto.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotBlankString } from "src/common/decorators/validation/IsNotBlankString";
+
 export class CreateDocumentRequestDto {
+  @ApiProperty()
+  @IsNotBlankString()
   public name: string;
 }
