77e342f3cd09ac5006fefc089c9ad96be0b4c0e5
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-24 04:22:50 -0300

    feat: add ApiResponse decorator for successful response in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpStatus } from "@nestjs/common";
+import { ApiResponse } from "@nestjs/swagger";
 
+import { ApiInternalServerResponse } from "./common/decorators/routes/docs";
+
+@ApiInternalServerResponse()
 @Controller()
 export class AppController {
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "Successful response",
+  })
   @Get()
   getHello(): string {
     return "Hello World!";
