3f9875317c8d04bfd06a0a9ebd4ddc85f98a326e
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-25 13:55:32 -0300

    feat: add @types/multer for file upload handling and enhance sendDocument method with file size validation

--- src/documents/providers/documents.service.ts
+++ src/documents/providers/documents.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, NotFoundException } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model, Types } from "mongoose";
@@ -12,7 +17,11 @@ import { v4 as uuidv4 } from "uuid";
 import { CreateDocumentRequestDto } from "../dto/request/create-document.dto";
 import { UpdateDocumentRequestDto } from "../dto/request/update-document.dto";
 import { PublicDocumentResponseDto } from "../dto/response/public-document.dto";
-import { Document, DocumentDocument } from "../schemas/document.schema";
+import {
+  Document,
+  DocumentDocument,
+  DocumentStatus,
+} from "../schemas/document.schema";
 @Injectable()
 export class DocumentsService {
   private readonly logger = new Logger(DocumentsService.name);
@@ -160,14 +169,25 @@ export class DocumentsService {
   }
 
   private generateDocumentUrl(mimeType: string): string {
-    const newFileName = uuidv4() + "." + mimeType;
+    const newFileName = uuidv4() + mimeType;
 
     return `${this.baseUrl}/${newFileName}`;
   }
 
   // DocumentFile is a placeholder for the actual file type
   // Replace it with the actual type used for document files
-  async sendDocument(documentId: string, documentFile: string): Promise<void> {
+  async sendDocument(
+    documentId: string,
+    documentFile: Express.Multer.File,
+  ): Promise<void> {
+    const fileSize = documentFile.size;
+
+    if (fileSize > 5 * 1024 * 1024) {
+      throw new BadRequestException(
+        `File size exceeds the limit of 5MB. Current size: ${fileSize} bytes.`,
+      );
+    }
+
     const document = (await this.documentModel
       .findById(documentId)
       .populate("employee")) as
@@ -178,16 +198,24 @@ export class DocumentsService {
       throw new NotFoundException(`Document with id ${documentId} not found`);
     }
 
+    // Check if the document is already sent
+    if (document.documentUrl) {
+      throw new BadRequestException(
+        `Document with id ${documentId} has already been sent. If you want to resend it, please delete the existing document and create a new one.`,
+      );
+    }
+
     // Logic to send the document (e.g., via email)
     // This is a placeholder for the actual implementation
     this.logger.log(`Sending document to employee ${document.employee.name}`);
     // Implement the actual
 
-    const mimeType = documentFile.split(".").pop()!;
-
-    const documentUrl = this.generateDocumentUrl(mimeType);
+    const documentUrl = this.generateDocumentUrl(documentFile.mimetype);
 
     document.documentUrl = documentUrl;
+
+    // Update document status to 'sent'
+    document.status = DocumentStatus.AVAILABLE;
 
     await document.save();
   }
