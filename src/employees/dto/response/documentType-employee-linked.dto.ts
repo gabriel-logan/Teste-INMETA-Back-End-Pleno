702c5d249111201bff051f4baf22ec0dd361cf56
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-25 12:20:01 -0300

    refactor: update ApiProperty decorator for documentTypeIdsLinked and documentTypeIdsUnlinked to specify type, format, and example

--- src/employees/dto/response/documentType-employee-linked.dto.ts
+++ src/employees/dto/response/documentType-employee-linked.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class DocumentTypeEmployeeLinkedResponseDto {
-  @ApiProperty({ type: [String] })
+  @ApiProperty({
+    type: String,
+    format: "objectId",
+    isArray: true,
+    example: ["60d5f484f1b2c8b1f8e4a3c1"],
+  })
   public documentTypeIdsLinked: string[];
 }
--- src/employees/dto/response/documentType-employee-unlinked.dto.ts
+++ src/employees/dto/response/documentType-employee-unlinked.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class DocumentTypeEmployeeUnlinkedResponseDto {
-  @ApiProperty({ type: [String] })
+  @ApiProperty({
+    type: String,
+    format: "objectId",
+    isArray: true,
+    example: ["60d5f484f1b2c8b1f8e4a3c1"],
+  })
   public documentTypeIdsUnlinked: string[];
 }
