bc5d953c687b3db929a0a51ca49c76ba9a3c8ab0
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-25 17:04:55 -0300

    fix: add temporary endpoint with parameter validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, HttpStatus } from "@nestjs/common";
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from "@nestjs/common";
 import { ApiResponse } from "@nestjs/swagger";
 
 import { ApiGlobalErrorResponses } from "./common/decorators/routes/docs";
@@ -18,5 +25,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return "Hello World!";
+  }
+
+  @Get("temp/a/b/c/:param")
+  getTemporary(@Param("param", ParseIntPipe) param: number): any {
+    if (param > 2) {
+      throw new BadRequestException(
+        "This endpoint is temporary and only accepts param values less than or equal to 2.",
+      );
+    }
+
+    return {
+      message: "Temporary endpoint response",
+      paramValue: param,
+    };
   }
 }
