3e15dc970218472fdcbacf3d11bc587f905409b0
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-26 10:59:13 -0300

    feat: add Employee decorator for extracting employee data from request

---
+++ src/common/decorators/routes/employee.decorator.ts
@@ -0,0 +1,14 @@
+import type { ExecutionContext } from "@nestjs/common";
+import { createParamDecorator } from "@nestjs/common";
+import type { Request } from "express";
+import type { AuthPayload } from "src/common/types";
+
+export const Employee = createParamDecorator(
+  (data: keyof AuthPayload | undefined, ctx: ExecutionContext) => {
+    const request: Request = ctx.switchToHttp().getRequest();
+
+    const { employee } = request;
+
+    return data ? employee?.[data] : employee;
+  },
+);
