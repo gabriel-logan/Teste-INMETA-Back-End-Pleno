74a76808a705f5afe07d812fe9afaaf6efb1f6c9
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-26 11:06:49 -0300

    feat: reintroduce AuthGuard with JWT authentication logic and integrate it into AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
 import { JwtModule } from "@nestjs/jwt";
 import envSecrets from "src/configs/env.secrets";
 import { EnvSecretConfig } from "src/configs/types";
 import { EmployeesModule } from "src/employees/employees.module";
 
 import { AuthController } from "./controllers/auth.controller";
+import { AuthGuard } from "./guards/auth.guard";
 import { AuthService } from "./providers/auth.service";
 
 @Module({
   imports: [
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule.forFeature(envSecrets)],
       useFactory: (configService: ConfigService<EnvSecretConfig, true>) => {
@@ -28,6 +31,12 @@ import { AuthService } from "./providers/auth.service";
     EmployeesModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AuthService,
+  ],
 })
 export class AuthModule {}
