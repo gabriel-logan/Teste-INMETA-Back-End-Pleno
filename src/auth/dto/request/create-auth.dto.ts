d98bd38167432a4769a803ea6000215f7416084b
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-26 10:32:19 -0300

    feat: enhance CreateAuthRequestDto with detailed ApiProperty descriptions for username and password

--- src/auth/dto/request/create-auth.dto.ts
+++ src/auth/dto/request/create-auth.dto.ts
@@ -3,13 +3,24 @@ import { MaxLength, MinLength } from "class-validator";
 import { IsNotBlankString } from "src/common/decorators/validation/IsNotBlankString";
 
 export class CreateAuthRequestDto {
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      "Username for authentication - For demo purposes it will be the CPF of the employee",
+    example: "12345678909",
+    maxLength: 40,
+    minLength: 3,
+  })
   @IsNotBlankString()
   @MaxLength(40)
   @MinLength(3)
   public username: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: "Password for authentication",
+    default: "123456",
+    maxLength: 255,
+    minLength: 6,
+  })
   @IsNotBlankString()
   @MinLength(6)
   @MaxLength(255)
