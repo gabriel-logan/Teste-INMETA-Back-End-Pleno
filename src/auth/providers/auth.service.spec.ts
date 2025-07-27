22983945ec56818730bf217788e2d281576b6f43
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-27 07:48:28 -0300

    test: enhance AuthService tests with bcrypt integration and additional scenarios

--- src/auth/providers/auth.service.spec.ts
+++ src/auth/providers/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { JwtModule } from "@nestjs/jwt";
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import * as bcrypt from "bcrypt";
 import { EmployeesService } from "src/employees/providers/employees.service";
 
 import { AuthService } from "./auth.service";
@@ -8,7 +9,14 @@ import { AuthService } from "./auth.service";
 describe("AuthService", () => {
   let service: AuthService;
 
-  const mockEmployeesService = {};
+  const mockEmployeesService = {
+    findByUsername: jest.fn().mockResolvedValue({
+      id: 1,
+      username: "testUser",
+      password: "hashedPassword",
+      role: "user",
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,5 +36,58 @@ describe("AuthService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should return a token", async () => {
+      const spyOnBcrypt = jest
+        .spyOn(bcrypt, "compare")
+        .mockResolvedValue(true as never);
+
+      const result = await service.signIn({
+        username: "testUser",
+        password: "testPassword",
+      });
+
+      expect(result).toHaveProperty("accessToken");
+      expect(result.accessToken).toBeDefined();
+      expect(result.accessToken).toEqual(expect.any(String));
+      expect(spyOnBcrypt).toHaveBeenCalledWith(
+        "testPassword",
+        "hashedPassword",
+      );
+      expect(spyOnBcrypt).toHaveBeenCalledTimes(1);
+    });
+
+    it("should throw UnauthorizedException for invalid username", async () => {
+      jest
+        .spyOn(mockEmployeesService, "findByUsername")
+        .mockRejectedValue(new Error("User not found"));
+
+      await expect(
+        service.signIn({
+          username: "invalidUser",
+          password: "testPassword",
+        }),
+      ).rejects.toThrow("Invalid username or password");
+    });
+
+    it("should throw UnauthorizedException for invalid password", async () => {
+      jest.spyOn(mockEmployeesService, "findByUsername").mockResolvedValue({
+        id: 1,
+        username: "testUser",
+        password: "hashedPassword",
+        role: "user",
+      });
+
+      jest.spyOn(bcrypt, "compare").mockResolvedValue(false as never);
+
+      await expect(
+        service.signIn({
+          username: "invalidUser",
+          password: "testPassword",
+        }),
+      ).rejects.toThrow("Invalid username or password");
+    });
   });
 });
