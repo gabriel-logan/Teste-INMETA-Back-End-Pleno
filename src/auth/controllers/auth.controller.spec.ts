3464b360f03b151fcded430a88c5ddfa62f179fc
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-27 07:40:08 -0300

    test: enhance AuthController and AuthService tests with mock implementations and improved setup

--- src/auth/controllers/auth.controller.spec.ts
+++ src/auth/controllers/auth.controller.spec.ts
@@ -7,16 +7,34 @@ import { AuthController } from "./auth.controller";
 describe("AuthController", () => {
   let controller: AuthController;
 
+  const mockAuthService = {
+    signIn: jest.fn().mockResolvedValue({
+      accessToken: "mockedAccessToken",
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [AuthService],
-    }).compile();
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should return an access token", async () => {
+      const createAuthDto = { username: "testUser", password: "testPass" };
+      const result = await controller.signIn(createAuthDto);
+      expect(result).toEqual({ accessToken: "mockedAccessToken" });
+      expect(mockAuthService.signIn).toHaveBeenCalledWith(createAuthDto);
+    });
   });
 });
--- src/auth/providers/auth.service.spec.ts
+++ src/auth/providers/auth.service.spec.ts
@@ -1,15 +1,27 @@
+import { JwtModule } from "@nestjs/jwt";
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { EmployeesService } from "src/employees/providers/employees.service";
 
 import { AuthService } from "./auth.service";
 
 describe("AuthService", () => {
   let service: AuthService;
 
+  const mockEmployeesService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      imports: [
+        JwtModule.register({
+          secret: "testSecret",
+        }),
+      ],
+      providers: [AuthService, EmployeesService],
+    })
+      .overrideProvider(EmployeesService)
+      .useValue(mockEmployeesService)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
   });
