e244afcad12d50c7332a09cf033c321de05b28a9
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-27 11:07:10 -0300

    test: update AppModule tests to use TestingModule for better isolation

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,7 +1,21 @@
+import { Test, type TestingModule } from "@nestjs/testing";
+
 import { AppModule } from "./app.module";
 
 describe("AppModule", () => {
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
   it("should be defined", () => {
-    expect(AppModule).toBeDefined();
+    expect(module).toBeDefined();
   });
 });
