7829e884ea44c020634557a10b984e31c8ce76d9
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-28 11:42:01 -0300

    test: add initial unit test for throttlerModuleOptions definition

---
+++ src/configs/tests/throttler-module-options.spec.ts
@@ -0,0 +1,7 @@
+import throttlerModuleOptions from "../throttler-module-options";
+
+describe("throttlerModuleOptions", () => {
+  it("should be defined", () => {
+    expect(throttlerModuleOptions).toBeDefined();
+  });
+});
