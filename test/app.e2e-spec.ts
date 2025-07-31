981057e0722e7c6fbc8f47020859712312a8cb87
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-07-31 02:03:29 -0300

    feat: add faker and cpf_and_cnpj-generator dependencies for enhanced data generation in tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
+import { faker } from "@faker-js/faker";
 import { HttpStatus, type INestApplication } from "@nestjs/common";
 import type { TestingModule } from "@nestjs/testing";
 import { Test } from "@nestjs/testing";
+import { generateCpf } from "cpf_and_cnpj-generator";
 import * as request from "supertest";
 import type { App } from "supertest/types";
 
@@ -27,5 +29,21 @@ describe("AppController (e2e)", () => {
       .get("/")
       .expect(HttpStatus.OK)
       .expect("Hello World!");
+  });
+
+  it("/admin-employees (POST)", async () => {
+    return await request(app.getHttpServer())
+      .post("/admin-employees")
+      .send({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        username: faker.internet.username(),
+        password: faker.internet.password(),
+        cpf: generateCpf(),
+      })
+      .expect(HttpStatus.CREATED)
+      .expect((res: request.Response) => {
+        expect(res.body).toHaveProperty("id");
+      });
   });
 });
