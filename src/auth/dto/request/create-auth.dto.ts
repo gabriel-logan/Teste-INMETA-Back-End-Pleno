f13966a3b140c4beb9d52b3427254b16adf1de56
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-08-01 08:34:54 -0300

    refactor: replace hardcoded validation constraints with constants in CreateAuthRequestDto

--- src/auth/dto/request/create-auth.dto.ts
+++ src/auth/dto/request/create-auth.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { MaxLength, MinLength } from "class-validator";
+import { validationConstraints } from "src/common/constants";
 import { IsNotBlankString } from "src/common/decorators/validation/IsNotBlankString";
 
 export class CreateAuthRequestDto {
@@ -7,22 +8,22 @@ export class CreateAuthRequestDto {
     description:
       "Username for authentication - For demo purposes it will be the CPF of the employee",
     example: "12345678909",
-    maxLength: 40,
-    minLength: 3,
+    maxLength: validationConstraints.employee.username.maxLength,
+    minLength: validationConstraints.employee.username.minLength,
   })
   @IsNotBlankString()
-  @MaxLength(40)
-  @MinLength(3)
+  @MaxLength(validationConstraints.employee.username.maxLength)
+  @MinLength(validationConstraints.employee.username.minLength)
   public username: string;
 
   @ApiProperty({
     description: "Password for authentication",
     default: "123456",
-    maxLength: 255,
-    minLength: 6,
+    maxLength: validationConstraints.employee.password.maxLength,
+    minLength: validationConstraints.employee.password.minLength,
   })
   @IsNotBlankString()
-  @MinLength(6)
-  @MaxLength(255)
+  @MaxLength(validationConstraints.employee.password.maxLength)
+  @MinLength(validationConstraints.employee.password.minLength)
   public password: string;
 }
